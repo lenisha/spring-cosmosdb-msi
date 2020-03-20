f634ef2019ed594ca19fe29914608b68e7d5e9b5
Elena Neroslavskaya <lenisha@gmail.com>
2020-03-20 13:17:03 -0400

    rest data

--- src/main/java/com/msft/cosmosmsi/CosmosMsiApplication.java
+++ src/main/java/com/msft/cosmosmsi/CosmosMsiApplication.java
@@ -1,22 +1,22 @@
 package com.msft.cosmosmsi;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CosmosMsiApplication implements CommandLineRunner {
+public class CosmosMsiApplication { //} implements CommandLineRunner {
 
-	@Autowired
-	private CustomerRepository repository;
+	//@Autowired
+	//private CustomerRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CosmosMsiApplication.class, args);
 	}
 
 
-	@Override
+	/*@Override
 	public void run(String... args) throws Exception {
 
 		repository.deleteAll();
@@ -44,5 +44,5 @@ public class CosmosMsiApplication implements CommandLineRunner {
 			System.out.println(customer);
 		}
 
-	}
+	} */
 }
--- src/main/java/com/msft/cosmosmsi/CustomerRepository.java
+++ src/main/java/com/msft/cosmosmsi/CustomerRepository.java
@@ -6,13 +6,17 @@
 
 package com.msft.cosmosmsi;
 
+
+
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "customer", path = "customer")
 public interface CustomerRepository extends MongoRepository<Customer, String> {
 
-	public Customer findByFirstName(String firstName);
-	public List<Customer> findByLastName(String lastName);
-
+	List<Customer> findByLastName(@Param("name") String name);
 }
+	
